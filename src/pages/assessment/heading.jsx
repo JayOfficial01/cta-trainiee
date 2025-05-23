da549bf147171822ebecc4aab95bc715bae3c2f2
Programmer-Dev125 <programmerdev125@gmail.com>
2025-05-22 19:32:11 -0500

    assessment sticky position

--- src/pages/assessment/heading.jsx
+++ src/pages/assessment/heading.jsx
@@ -1,7 +1,7 @@
 export default function Heading({ answered, total }) {
   const percentage = Math.floor((answered / total) * 100);
   return (
-    <div className="w-[95%] sticky top-0 mx-auto flex flex-col md:flex-row justify-between items-center mt-10">
+    <div className="w-[95%] sticky top-20 mx-auto flex flex-col md:flex-row justify-between items-center mt-10">
       <div className="bg-[#007F5F] w-[100%] md:w-[49.5%] text-white p-12 rounded-md"></div>
 
       <div className="bg-[#007F5F] w-[100%] mt-5 md:mt-0 md:w-[49.5%] p-4 rounded-md flex flex-row gap-7 items-center">
